5eb12b82af6b3b023fd4c920af43244effe6cea5
Declan <dec1@geoaze.com>
2019-01-29 21:24:17 +0100

    better fix: test for specific files rather than containnig dir (since eg lib (a well as lib64) exists for "arm64-v8a" build, but doesn't contain all src files for cp

--- build.sh
+++ build.sh
@@ -223,13 +223,13 @@ cd $BUILDDIR/$ARCH
 			
 	for f in libicudata libicutest libicui18n libicuio libicutu libicuuc; do
 		if [ $SHARED_ICU ]; then
-			[[ -d ../../lib64 ]] && cp -f -H ../../lib64/$f.so ../../   # Maybe it's here, maybe not, who knows
-			[[ -d ../../lib32 ]] && cp -f -H ../../lib32/$f.so ../../   
-			[[ -d ../../lib   ]] && cp -f -H ../../lib/$f.so ../../   
+			[[ -f ../../lib64/$f.so ]] && cp -f -H ../../lib64/$f.so ../../   # Maybe it's here, maybe not, who knows
+			[[ -f ../../lib32/$f.so ]] && cp -f -H ../../lib32/$f.so ../../   
+			[[ -f ../../lib/$f.so   ]] && cp -f -H ../../lib/$f.so ../../   
 		else
-			[[ -d ../../lib64 ]] && cp -f ../../lib64/$f.a ../../      # Different libtool versions do things differently
-			[[ -d ../../lib32 ]] && cp -f ../../lib32/$f.a ../../     
-			[[ -d ../../lib   ]] && cp -f ../../lib/$f.a ../../       
+			[[ -f ../../lib64/$f.a ]] && cp -f ../../lib64/$f.a ../../      # Different libtool versions do things differently
+			[[ -f ../../lib32/$f.a ]] && cp -f ../../lib32/$f.a ../../     
+			[[ -f ../../lib/$f.a   ]] && cp -f ../../lib/$f.a ../../       
 		fi
 	done
 
@@ -377,10 +377,9 @@ cd $BUILDDIR/$ARCH
 		make V=1 install || exit 1
 
 	for f in libicudata libicutest libicui18n libicuio libicutu libicuuc libiculx; do
-		[[ -d ../../lib   ]] && cp -f ../../lib/$f.a ../../
-		[[ -d ../../lib64 ]] && cp -f ../../lib64/$f.a ../../
-		[[ -d ../../lib32 ]] && cp -f ../../lib32/$f.a ../../
-		exit 1
+		[[ -f ../../lib/$f.a   ]] && cp -f ../../lib/$f.a ../../
+		[[ -d ../../lib64/$f.a ]] && cp -f ../../lib64/$f.a ../../
+		[[ -d ../../lib32/$f.a ]] && cp -f ../../lib32/$f.a ../../
 	 done
 
 } || exit 1
